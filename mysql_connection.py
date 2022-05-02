826bb41b8876c2cadb81901ab305028b9a67a584
jisu0427 <93955242+jisu0427@users.noreply.github.com>
2022-05-02 09:25:45 +0900

    엔드포인트 private

--- mysql_connection.py
+++ mysql_connection.py
@@ -3,7 +3,7 @@ from mysql.connector.errors import Error
 
 def get_connection():
     connection = mysql.connector.connect(
-            host = 'database-1.ckgggmo40ahf.us-east-2.rds.amazonaws.com',
+            host = 'AWS 엔드포인트 주소',
             database = 'streamlit_db',
             user = 'python_user',
             password='2105'
